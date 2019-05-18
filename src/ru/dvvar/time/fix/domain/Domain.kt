0a4445a543073dcfed10f618d5af5f6f5acefa95
dmitriy_varygin <dmitriy.varygin@yandex.ru>
2019-05-18 12:07:47 +0300

    add time fixation in activity

--- src/ru/dvvar/time/fix/domain/Domain.kt
+++ src/ru/dvvar/time/fix/domain/Domain.kt
@@ -1,8 +1,13 @@
 package ru.dvvar.time.fix.domain
 
-typealias Activity = String
+import java.time.LocalDate
+import java.util.*
+
+data class Activity(val name: String, val minutesInDate: SortedMap<LocalDate, Minute>)
 
 data class User(val id: Int, val username: String, val activities: Set<Activity>)
+
+typealias Minute = Int
 
 fun changeUsername(user: User, newUserName: String): User =
     User(user.id, newUserName, user.activities)
@@ -12,6 +17,12 @@ fun addActivity(user: User, activity: Activity): User =
 
 fun removeActivity(user: User, activity: Activity): User =
     changeActivities(user) { it - activity }
+
+fun addTimeToActivity(activity: Activity, date: LocalDate, minutes: Minute): Activity {
+    val mapCopy = TreeMap<LocalDate, Minute>(activity.minutesInDate)
+    mapCopy.merge(date, minutes) { old, new -> old + new }
+    return Activity(activity.name, mapCopy)
+}
 
 private fun changeActivities(user: User, change: (Set<Activity>) -> Set<Activity>): User =
     User(user.id, user.username, change(user.activities))
