e9fc1744cfac2e059734772a1af0c9ed020f2edd
dmitriy_varygin <dmitriy.varygin@yandex.ru>
2019-05-18 12:21:15 +0300

    add server (ktor)

---
+++ src/ru/dvvar/time/fix/transport/Server.kt
@@ -0,0 +1,18 @@
+package ru.dvvar.time.fix.transport
+
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun createAndStartServer() {
+    val server = embeddedServer(Netty, port = 4444) {
+        routing {
+            get("/") { call.respondText("Hello, world!", ContentType.Text.Plain) }
+        }
+    }
+    server.start(wait = true)
+}
