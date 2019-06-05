e3689febad6dcaf90043c1a6357b74053a4743b3
dmitriy_varygin <dmitriy.varygin@yandex.ru>
2019-06-05 08:37:04 +0300

    add rest endpoint for fixate_time by activity

--- src/ru/dvvar/time/fix/port/rest/ActivityPort.kt
+++ src/ru/dvvar/time/fix/port/rest/ActivityPort.kt
@@ -1,12 +1,14 @@
 package ru.dvvar.time.fix.port.rest
 
-import ru.dvvar.time.fix.bl.Result
+import ru.dvvar.time.fix.bl.BooleanResult
 
 data class ChangeActivity(val userId: Int, val activityName: String)
 data class RenameActivity(val userId: Int, val oldActivityName: String, val newActivityName: String)
+data class FixateTime(val userId: Int, val activityName: String, val minutes: Int)
 
 interface ActivityPort {
-    fun addActivity(changeActivity: ChangeActivity): Result<Boolean>
-    fun removeActivity(changeActivity: ChangeActivity): Result<Boolean>
-    fun renameActivity(renameActivity: RenameActivity): Result<Boolean>
+    fun addActivity(changeActivity: ChangeActivity): BooleanResult
+    fun removeActivity(changeActivity: ChangeActivity): BooleanResult
+    fun renameActivity(renameActivity: RenameActivity): BooleanResult
+    fun fixateTime(fixateTime: FixateTime): BooleanResult
 }
--- src/ru/dvvar/time/fix/transport/ActivityServer.kt
+++ src/ru/dvvar/time/fix/transport/ActivityServer.kt
@@ -10,6 +10,7 @@ import io.ktor.routing.post
 import io.ktor.routing.put
 import ru.dvvar.time.fix.port.rest.ActivityPort
 import ru.dvvar.time.fix.port.rest.ChangeActivity
+import ru.dvvar.time.fix.port.rest.FixateTime
 import ru.dvvar.time.fix.port.rest.RenameActivity
 
 class ActivityServer(private val activityPort: ActivityPort) : AbstractServer() {
@@ -27,6 +28,11 @@ class ActivityServer(private val activityPort: ActivityPort) : AbstractServer() 
         put("$VERSION_V1_PREFIX/rename_activity") {
             val renamingActivity = call.receive<RenameActivity>()
             val result = activityPort.renameActivity(renamingActivity)
+            call.respond(HttpStatusCode.OK, result)
+        }
+        post("$VERSION_V1_PREFIX/fixate_time") {
+            val fixateTime = call.receive<FixateTime>()
+            val result = activityPort.fixateTime(fixateTime)
             call.respond(HttpStatusCode.OK, result)
         }
     }
